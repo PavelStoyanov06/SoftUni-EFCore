e823c0dc1a7986a584811af1c1a150f1156dc7a4
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2023-11-26 21:52:23 +0200

    Entity Framework Core

---
+++ Solutions/EntityFrameworkCoreIntroduction/SoftUni/Program.cs
@@ -0,0 +1,13 @@
+﻿using SoftUni;
+using SoftUni.Data;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        using SoftUniContext context = new SoftUniContext();
+
+        StartUp.GetEmployeesFromResearchAndDevelopment(context);
+    }
+}
---
+++ Solutions/EntityFrameworkCoreIntroduction/SoftUni/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using SoftUni.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            string result = string.Empty;
+            var employees = context.Employees.ToList().OrderBy(x => x.Salary);
+
+            foreach (var employee in employees)
+            {
+                result += $"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}{Environment.NewLine}";
+            }
+
+            return result;
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder content = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(x => x.Salary > 50000)
+                .OrderBy(x => x.FirstName)
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                content.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            return content.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder content = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(x => x.Department.DepartmentId == 6)
+                .OrderBy(x => x.Salary)
+                .ThenByDescending(x => x.FirstName);
+
+            foreach (var employee in employees)
+            {
+                content.AppendLine($"{employee.FirstName} {employee.LastName} " +
+                                   $"from {employee.Department.Name} - ${employee.Salary:F2}");
+            }
+
+            return content.ToString().TrimEnd();
+        }
+    }
+}
